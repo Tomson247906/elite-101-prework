a3352ccd916e1fe062c857924df5c05f890071ac
Tomson247906 <damian.cardnoa@gmail.com>
2025-09-15 22:41:06 +0000

    adding something

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("hello im a food chat bot.")
 name = input("what is your name:")
 age = input ("hello welcome "+ name + ",how old are you:")
+#if age == 18 : 
+    #print ("are you starting collage or in highschool:")
+#else: 
+    #print ("hi there");
